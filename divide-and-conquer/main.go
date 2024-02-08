6276de03778f34aab8091c3558c3f9ccc91fd83f
locpham <loc.pham2412@gmail.com>
2024-02-08 10:11:58 +0000

    quick sort with random pivot

--- divide-and-conquer/main.go
+++ divide-and-conquer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 // Input: 2 4 6
@@ -76,8 +77,59 @@ func QuickSort(input []int) []int {
 	return QuickSortRecursion(input)
 }
 
+func Swap(input []int, a, b int) []int {
+	temp := input[a]
+	input[a] = input[b]
+	input[b] = temp
+
+	return input
+}
+
+// TODO: Should sort in-place, means should not use auxiliary arrays, it will help to reduce space complexity
+func RandomizedPartition(arr []int, low, high int) ([]int, int) {
+	var pivotIndex int = rand.Intn(high-low) + low
+	pivot := arr[pivotIndex]
+
+	var less, greater []int
+	for k, val := range arr {
+		if val < pivot {
+			less = append(less, val)
+		} else {
+			if k == pivotIndex {
+				continue
+			}
+			greater = append(greater, val)
+		}
+	}
+
+	result := append(less, pivot)
+	result = append(result, greater...)
+	return result, pivotIndex
+}
+
+func RandomizedQuickSortRecursion(input []int, low, high int) []int {
+	if high <= low {
+		return input
+	}
+
+	input, pivotIndex := RandomizedPartition(input, low, high)
+
+	input = RandomizedQuickSortRecursion(input, low, pivotIndex-1)
+	input = RandomizedQuickSortRecursion(input, pivotIndex+1, high)
+
+	return input
+}
+
+func RandomizedQuickSort(input []int) []int {
+	return RandomizedQuickSortRecursion(input, 0, len(input)-1)
+}
+
+// TODO: write test case
 func main() {
 	fmt.Println("SumOfArrayRecursion: ", SumOfArrayRecursion([]int{1, 2, 3, 4, 5}))
 	fmt.Println("BinarySearch: ", BinarySearch(13, []int{2, 4, 6, 8, 10, 11, 12, 13, 14}))
-	fmt.Println("QuickSort: ", QuickSort([]int{5, 4, 6, 2, 3, 2, 11, 7, 1, 8, 2, 2}))
+	fmt.Println("QuickSort-FixedPivot-Unsorted-Duplicate: ", QuickSort([]int{5, 4, 6, 2, 3, 2, 11, 7, 1, 8, 2, 2}))
+	fmt.Println("QuickSort-FixedPivot-Sorted: ", QuickSort([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}))
+	fmt.Println("QuickSort-PivotRandom-Sorted: ", RandomizedQuickSort([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}))
+	fmt.Println("QuickSort-PivotRandom-Unsorted-Duplicate: ", QuickSort([]int{5, 4, 6, 2, 3, 2, 11, 7, 1, 8, 2, 2}))
 }
